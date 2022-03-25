4f7454862848371181eecb745b16c27ba6d9fe36
Darren Tham <95648134+Darren-Tham@users.noreply.github.com>
2022-03-24 22:53:43 -0400

    Updated script.js
    Logic completed only going up

--- script.js
+++ script.js
@@ -38,9 +38,18 @@ function randomTile() {
     tileChild.innerText = Math.random() <= 0.1 ? "4" : "2";
     addColor(tileChild);
     tile.append(tileChild);
-    setTimeout(() => tileChild.className = "tile", DURATION);
+    setTimeout(() => (tileChild.className = "tile"), DURATION);
   }
 } // randomTile
+
+// Adds a random tile after each move
+function addRandomTileAfterMoves (maxDelay, validMove) {
+  if (!validMove) return;
+
+  setTimeout(() => {
+    randomTile();
+  }, maxDelay)
+}
 
 function test() {
   const child = document.createElement("div");
@@ -48,21 +57,22 @@ function test() {
   child.innerText = "2";
   addColor(child);
   tiles[0][0].append(child);
+  setTimeout(() => (child.className = "tile"), DURATION);
 }
 
 function test2() {
   const child = document.createElement("div");
   child.classList.add("tile", "popout");
-  child.innerText = "2";
+  child.innerText = "4";
   addColor(child);
   tiles[1][0].append(child);
+  setTimeout(() => (child.className = "tile"), DURATION);
 }
 
 // Setup the board using setup() and add two random tiles in the board
 // If two tiles are 4, then the game will restart
 function begin() {
   setup();
-
   randomTile();
   randomTile();
   /*
@@ -96,35 +106,85 @@ function addColor(tile) {
   tile.style.background = "hsl(200, 100%, " + (100 - +tile.innerText * 5) + "%)";
 } // addColor
 
+// Adds the slide effect to the tile
+function addSlide(tile, distance) {
+  switch (distance) {
+    case 1:
+      tile.classList.add("slide-one");
+      break;
+    case 2:
+      tile.classList.add("slide-two");
+      break;
+    case 3:
+      tile.classList.add("slide-three");
+  } // switch
+} // addSlide
+
+// Updates the new merged tile
+function newMergedTile(tile, delay) {
+  setTimeout(() => {
+    const tileChild = tile.lastChild;
+    tileChild.classList.add("merge");
+    tileChild.innerText *= 2;
+    addColor(tileChild);
+    tile.removeChild(tile.firstChild);
+  }, delay); // setTimeout
+} // newMergedTile
+
+// Resets the element's class name to only tile for aesthetic
+function clearClassName(tile, delay, overlap) {
+  if (overlap) {
+    delay += DURATION;
+  } // if
+
+  setTimeout(() => {
+    tile.className = "tile";
+  }, delay); // setTimeout
+} // clearClassName
+
+function isTile(row, col) {
+  return !tiles[row][col].firstChild;
+}
+
 window.addEventListener("keydown", (e) => {
   switch (e.key) {
     case "w":
     case "W":
     case "ArrowUp":
+      let maxDelay = 0;
+      let validMove = false;
       for (let col = 0; col < COLS; col++) {
         for (let row = 1; row < ROWS; row++) {
           // if it is not a tile, continue
-          if (!tiles[row][col].firstChild) {
+          if (isTile(row, col)) {
             continue;
           } // if
 
+
           const currentTile = tiles[row][col];
-          let currentRow = row;
+          let newRow = row;
           let overlap = false;
 
           // while loop breaks when it reaches the edge of the board
           // or when it reaches a tile
-          while (currentRow !== 0 && !tiles[currentRow - 1][col].firstChild) {
-            currentRow--;
+          while (newRow !== 0 && !tiles[newRow - 1][col].firstChild) {
+            newRow--;
           } // while
 
           // if the tile above is the same number, make the tile above the newTile
-          if (currentRow !== 0 && tiles[currentRow - 1][col].firstChild?.innerText === currentTile.firstChild?.innerText) {
-            currentRow--;
+          if (newRow !== 0 && tiles[newRow - 1][col].firstChild?.innerText === currentTile.firstChild.innerText) {
+            newRow--;
             overlap = true;
           } // if
 
-          const newTile = tiles[currentRow][col];
+          const distance = row - newRow;
+          if (distance === 0) {
+            continue;
+          } else {
+            validMove = true;
+          } // if-else
+
+          const newTile = tiles[newRow][col];
           const newTileChild = document.createElement("div");
           newTileChild.classList.add("tile");
           newTileChild.innerText = currentTile.firstChild.innerText;
@@ -138,40 +198,19 @@ window.addEventListener("keydown", (e) => {
           document.documentElement.style.setProperty("--TWO", "translateY(calc((var(--GAP) + var(--SIZE)) * 2))");
           document.documentElement.style.setProperty("--THREE", "translateY(calc((var(--GAP) + var(--SIZE)) * 3))");
 
-          let delay;
-
-          switch (row - currentRow) {
-            case 1:
-              newTileChild.classList.add("slide-one");
-              delay = DURATION;
-              break;
-            case 2:
-              newTileChild.classList.add("slide-two");
-              delay = DURATION * 2;
-              break;
-            case 3:
-              newTileChild.classList.add("slide-three");
-              delay = DURATION * 3;
-          } // switch
-
-          // CHANGE TO CORRECT COLOR AND CHANGE NUMBER. REMOVE OLD CHILD AND CLEAR CLASS TAGS
-          // FIX TRANSITION
-          // I WAS REALLY TIRED AND WENT TO SLEEP BECAUSE BADMINTON
-          // GOOD LUCK FUTURUE ME!!! YOU GOT THIS :)
+          let delay = DURATION * (distance);
+          if (delay > maxDelay) {
+            maxDelay = delay;
+          } // if
+          addSlide(newTileChild, distance);
 
           if (overlap) {
-            setTimeout(() => {
-              newTileChild.classList.add("merge");
-              newTileChild.innerText *= 2;
-              addColor(newTileChild);
-              newTile.removeChild(newTile.firstChild);
-            }, delay);
-            setTimeout(() => {
-              newTileChild.className = "tile";
-            }, delay + DURATION);
+            newMergedTile(newTile, delay);
           } // if
+          clearClassName(newTileChild, delay, overlap);
         } // for
       } // for
+      addRandomTileAfterMoves(maxDelay, validMove);
       break;
     case "a":
     case "A":
