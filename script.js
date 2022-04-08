23313e64014d56279b00f734d9640a4796e9ae22
Darren Tham <95648134+Darren-Tham@users.noreply.github.com>
2022-04-08 01:31:17 -0400

    Added Settings and changed board colors

--- script.js
+++ script.js
@@ -1,53 +1,87 @@
 // IDEAS
 // ADD WIN/LOSE GAMESCREEN
 // ADD SCORE
-// MAKE SETTING TAB (CHANGE COLOR, CHANGE SIZE, CHANGE DURATION, CHANGE NUMBER OF TILES SPAWNING)
-
-const ROWS = 4
-const COLS = 4
-const DURATION = 100
 
 class Grid {
 	// Instance Variables:
 	// gridElement
 	// tiles
+	// rows
+	// cols
+	// duration
+	// numTiles
 	// keyPressed
 
-	constructor(gridElement) {
-		this.gridElement = gridElement
-		this.tiles = new Array(ROWS).fill().map(() => new Array(COLS).fill(null))
+	constructor(rows, cols, duration, numTiles) {
+		// this.gridElement = gridElement
+		this.gridElement = document.createElement('div')
+		this.gridElement.id = 'grid'
+		document.body.append(this.gridElement)
 		this.keyPressed = false
-		this.emptyCells()
-
-		gridElement.style.setProperty('--ROWS', ROWS)
-		gridElement.style.setProperty('--COLS', COLS)
-		gridElement.style.setProperty('--DURATION', `${DURATION}ms`)
+		this.rows = rows
+		this.cols = cols
+		this.numTiles = numTiles
+		this.tiles = new Array(rows).fill().map(() => new Array(cols).fill(null))
+		this.setDuration(duration)
+		this.gridElement.style.setProperty('--ROWS', this.rows)
+		this.gridElement.style.setProperty('--COLS', this.cols)
 	} // constructor
+
+	// Starts the 2048 game!
+	start() {
+		this.emptyCells()
+		this.setSize()
+		this.randomTiles(true)
+		addEventListener('keydown', eventListener)
+	} // start
+
+	// Deletes the grid
+	delete() {
+		this.gridElement.remove()
+	} // delete
+
+	// Sets the duration of the animation
+	setDuration(duration) {
+		this.duration = duration
+		this.gridElement.style.setProperty('--DURATION', `${this.duration}ms`)
+	} // setDuration
+
+	// Sets the size of each tile
+	setSize() {
+		const VIEWPORT = 75
+		if (this.rows > this.cols) {
+			this.gridElement.style.setProperty('--SIZE', `calc(${VIEWPORT}vmin / var(--ROWS))`)
+		} else {
+			this.gridElement.style.setProperty('--SIZE', `calc(${VIEWPORT}vmin / var(--COLS))`)
+		} // if-else
+	} // setSize
 
 	// Fill the grid with empty gray cells (placeholders)
 	emptyCells() {
-		for (let i = 0; i < ROWS * COLS; i++) {
+		for (let i = 0; i < this.rows * this.cols; i++) {
 			const cell = document.createElement('div')
 			cell.classList.add('cell')
 			this.gridElement.append(cell)
 		} // for
 	} // emptyCells
 
-	// Spawns a random tile in the grid
-	randomTile() {
-		const availableCells = []
-		this.tiles.forEach((row, y) => {
-			row.forEach((tile, x) => {
-				if (!tile) {
-					availableCells.push({ x, y })
-				} // if
+	// Spawns random tiles in the grid
+	randomTiles(start = false) {
+    const tilesPerMove = start ? 2 : this.numTiles
+		for (let i = 0; i < tilesPerMove; i++) {
+			const availableCells = []
+			this.tiles.forEach((row, y) => {
+				row.forEach((tile, x) => {
+					if (!tile) {
+						availableCells.push({ x, y })
+					} // if
+				}) // forEach
 			}) // forEach
-		}) // forEach
-		const randomCoord = availableCells[Math.floor(Math.random() * availableCells.length)]
-
-		const randomTile = new Tile(randomCoord.x, randomCoord.y)
-		this.tiles[randomCoord.y][randomCoord.x] = randomTile
-		this.gridElement.append(randomTile.tileElement)
+			const randomCoord = availableCells[Math.floor(Math.random() * availableCells.length)]
+			const randomTile = new Tile(randomCoord.x, randomCoord.y)
+			this.tiles[randomCoord.y][randomCoord.x] = randomTile
+			this.gridElement.append(randomTile.tileElement)
+		} // for
 	} // randomTile
 
 	// Removes most properties from tiles to keep clean
@@ -64,7 +98,7 @@ class Grid {
 					}
 				}) // map
 			}) // forEach
-		}, DURATION) // setTimeout
+		}, this.duration) // setTimeout
 	} // resetProperties
 
 	// Checks if the tile is in bounds
@@ -75,9 +109,9 @@ class Grid {
 			case 'left':
 				return newDim && !this.tiles[row][newDim - 1]
 			case 'down':
-				return newDim < ROWS - 1 && !this.tiles[newDim + 1][col]
+				return newDim < this.rows - 1 && !this.tiles[newDim + 1][col]
 			case 'right':
-				return newDim < COLS - 1 && !this.tiles[row][newDim + 1]
+				return newDim < this.cols - 1 && !this.tiles[row][newDim + 1]
 		} // switch
 	} // checkInBounds
 
@@ -93,10 +127,10 @@ class Grid {
 				nextTile = newDim - 1 >= 0 ? this.tiles[row][newDim - 1] : null
 				break
 			case 'down':
-				nextTile = newDim + 1 < ROWS ? this.tiles[newDim + 1][col] : null
+				nextTile = newDim + 1 < this.rows ? this.tiles[newDim + 1][col] : null
 				break
 			case 'right':
-				nextTile = newDim + 1 < COLS ? this.tiles[row][newDim + 1] : null
+				nextTile = newDim + 1 < this.cols ? this.tiles[row][newDim + 1] : null
 				break
 		} // switch
 		return currentTile.tileElement.innerText === nextTile?.tileElement.innerText && !nextTile.tileElement.classList.contains('merge')
@@ -104,7 +138,7 @@ class Grid {
 
 	// Get new dimension of tile
 	getNewDim(direction, row, col) {
-    let newDim = direction === 'up' || direction === 'down' ? row : col
+		let newDim = direction === 'up' || direction === 'down' ? row : col
 		while (this.inBounds(direction, newDim, row, col)) {
 			newDim = direction === 'down' || direction === 'right' ? newDim + 1 : newDim - 1
 		} // while
@@ -166,7 +200,7 @@ class Grid {
 		const dist = this.getDist(direction, row, col, newDim)
 		if (!dist) return { delay: 0, validMove: false }
 
-		const delay = DURATION * dist
+		const delay = this.duration * dist
 		this.applyChanges(direction, row, col, newDim, delay, dist)
 		return { delay, validMove: true }
 	} // slide
@@ -177,9 +211,9 @@ class Grid {
 			case 'left':
 				return 1
 			case 'down':
-				return ROWS - 2
+				return this.rows - 2
 			case 'right':
-				return COLS - 2
+				return this.cols - 2
 		} // switch
 	} // setInitialization
 
@@ -187,9 +221,9 @@ class Grid {
 	conditionCheck(direction, j) {
 		switch (direction) {
 			case 'up':
-				return j < ROWS
+				return j < this.rows
 			case 'left':
-				return j < COLS
+				return j < this.cols
 			case 'down':
 			case 'right':
 				return j >= 0
@@ -204,10 +238,10 @@ class Grid {
 		let maxDelay = 0
 		let validMove = false
 
-		const dim = direction === 'up' || direction === 'down' ? COLS : ROWS
-		let j = this.setInitialization(direction)
+		const dim = direction === 'up' || direction === 'down' ? this.cols : this.rows
 
 		for (let i = 0; i < dim; i++) {
+			let j = this.setInitialization(direction)
 			while (this.conditionCheck(direction, j)) {
 				const obj = direction === 'up' || direction === 'down' ? this.slide(direction, j, i) : this.slide(direction, i, j)
 				maxDelay = Math.max(maxDelay, obj.delay)
@@ -218,13 +252,12 @@ class Grid {
 					j--
 				} // if-else
 			} // while
-			j = this.setInitialization(direction)
 		} // for
 		setTimeout(() => {
 			if (validMove) {
-				this.randomTile()
+				this.randomTiles()
 				this.resetProperties()
-				setTimeout(() => (this.keyPressed = false), DURATION)
+				setTimeout(() => (this.keyPressed = false), this.duration)
 			} else {
 				this.keyPressed = false
 			} // if-else
@@ -258,7 +291,7 @@ class Tile {
 
 	// Adds a color to a tile
 	addColor() {
-		const num = 100 - Math.log(this.tileElement.innerText) * 5
+		const num = 100 - Math.log2(this.tileElement.innerText) * 9.5
 		this.tileElement.style.background = `hsl(200, 100%, ${num}%)`
 	} // addColor
 
@@ -284,33 +317,83 @@ class Tile {
 	} // remove
 } // Tile
 
+// ------------------------------ Settings Setup ------------------------------
+
+const SETTINGS_DELAY = 100
+const settingsPopup = document.querySelector('.settings-popup')
+
+// Pops up a window of the settings
+function openSettings() {
+	const wrapper = document.querySelector('.settings-wrapper')
+	wrapper.addEventListener('click', () => {
+		settingsPopup.style.visibility = 'visible'
+		settingsPopup.style.animation = `pop ${SETTINGS_DELAY}ms`
+		window.removeEventListener('keydown', eventListener)
+	}) // addEventListener
+} // openSettings
+
+// Closes and updates the settings
+function closeAndUpdateSettings() {
+	const close = document.querySelector('.close')
+	close.addEventListener('click', () => {
+		settingsPopup.style.animation = `shrink ${SETTINGS_DELAY}ms`
+		setTimeout(() => (settingsPopup.style.visibility = 'hidden'), SETTINGS_DELAY)
+		window.addEventListener('keydown', eventListener)
+
+		const rows = +document.getElementById('rows').value
+		const cols = +document.getElementById('cols').value
+		const duration = +document.getElementById('duration').value
+		const numTiles = +document.getElementById('num-tiles').value
+
+		if (dimChanged(rows, cols)) {
+			grid.delete()
+			grid = new Grid(rows, cols, duration, numTiles)
+			grid.start()
+		} else {
+			grid.setDuration(duration)
+      grid.numTiles = numTiles
+		} // if-else
+	}) // addEventListener
+} // closeSettings
+
+// Checks to see if the dimension of the grid has changed
+// If it has, the game will restart
+function dimChanged(newRows, newCols) {
+	return !(newRows === grid.rows && newCols === grid.cols)
+} // settingsChanged
+
 // ------------------------------ Begin Program ------------------------------
 
-const grid = new Grid(document.getElementById('grid'))
-grid.randomTile()
-grid.randomTile()
+let grid = new Grid(4, 4, 75, 1)
 
-addEventListener('keydown', e => {
+// Makes the game play
+function play() {
+	grid.start()
+	openSettings()
+	closeAndUpdateSettings()
+} // play
+
+// Sets up the EventListener for addEventListener
+// Will be used for removeEventListener
+function eventListener(e) {
 	switch (e.key) {
 		case 'w':
 		case 'W':
 		case 'ArrowUp':
-			grid.update('up')
-			break
+			return grid.update('up')
 		case 'a':
 		case 'A':
 		case 'ArrowLeft':
-			grid.update('left')
-			break
+			return grid.update('left')
 		case 's':
 		case 'S':
 		case 'ArrowDown':
-			grid.update('down')
-			break
+			return grid.update('down')
 		case 'd':
 		case 'D':
 		case 'ArrowRight':
-			grid.update('right')
-			break
+			return grid.update('right')
 	} // switch
-}) // addEventListener
+} // eventListener
+
+play()
